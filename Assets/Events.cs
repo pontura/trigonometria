a960dfdccc06ca92c6751268cf68fdb2ee58fd1b
pontura <pontura@gmail.com>
2018-05-16 17:42:01 -0300

    merge corregido

--- Assets/Events.cs
+++ Assets/Events.cs
@@ -1,16 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-<<<<<<< HEAD
 using System.Collections.Generic;
 
 public static class Events {
 	
-	public static System.Action<UIButton> OnButtonClickd = delegate { };
-=======
-
-public static class Events {
-	
+	public static System.Action<UIButton> OnButtonClickd = delegate { };	
 	public static System.Action<GameObject> OnMouseCollide = delegate { };
 
->>>>>>> 8c4b3e7957f93bd6c13155c168da7488f3d410ca
 }
